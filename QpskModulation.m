ad824a6d7213a7ace8bdb3569f138b80a61ba90f
D.mon 24 <dchaminov@ya.ru>
2024-08-05 11:21:27 +0300

    comments

--- QpskModulation.m
+++ QpskModulation.m
@@ -1,4 +1,6 @@
 function data=QpskModulation(bits)
+    % modulates bits to QPSK complex amplitudes
+    % `bits` must be even-length
     bits=reshape(bits,2,[]);
     data=(1-2*bits(1,:)+1j*(1-2*bits(2,:)))/sqrt(2);
 end
